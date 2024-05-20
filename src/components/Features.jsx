33d241b30c2b8a7433a7a596764b1504a11fc92b
kimtony123 <kimtony571@gmail.com>
2024-05-20 16:10:06 +0300

    Third commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,7 +8,7 @@ const FeatureSection = () => {
           Features.
         </span>
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
-          Areweave's {" "}
+          Arweave's {" "}
           <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
             City Square.
           </span>
